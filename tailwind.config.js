9f62d4e424041524c46a10345cda580ee0d5e2fa
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-09-27 12:42:11 +0530

    navbar fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        //Colors
+        white: "#FFFFFF",
+        primary: "#748FFC",
+        //Tints
+        headerColor: "#BAC8FF", //Opacity should be 20%
+        loginSidebar: "#748FFC", //Opacity should be 10%
+        //Shades
+      },
+    },
 
-    },
-    colors: {
-      //Colors
-      white: "#FFFFFF",
-      primary: "#748FFC",
-      //Tints
-      headerColor: "#BAC8FF", //Opacity should be 20%
-      loginSidebar: "#748FFC", //Opacity should be 10%
-      //Shades
-    },
   },
   plugins: [],
 };
