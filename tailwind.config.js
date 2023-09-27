a322f3f6e9b61346f7a069137c1790d261369259
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2023-09-27 12:09:50 +0530

    signup need to add css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        //Colors
-        white: "#FFFFFF",
-        "primary-color": "748FFC",
-        //Tints
-        "header-color": "BAC8FF", //Opacity should be 20%
-        "login-sidebar": "748FFC", //Opacity should be 10%
-        //Shades
-      },
+
+    },
+    colors: {
+      //Colors
+      white: "#FFFFFF",
+      primary: "#748FFC",
+      //Tints
+      headerColor: "#BAC8FF", //Opacity should be 20%
+      loginSidebar: "#748FFC", //Opacity should be 10%
+      //Shades
     },
   },
   plugins: [],
